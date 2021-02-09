af0607c1f18a56bdaa6d5e653226e8807fdd173a
James Kirwan <james.kirwan1@ucdconnect.ie>
2021-02-09 19:31:17 +0000

    created flight.sql and flight java class

---
+++ src/main/java/ie/ucd/dfh/model/Flight.java
@@ -0,0 +1,61 @@
+package ie.ucd.dfh.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Entity
+@Table(name = "flights")
+public class Flight {
+    @Id@GeneratedValue(strategy = GeneratedValue.IDENTITY)
+    private Long flightId;
+
+    @NotBlank
+    private String departure;
+
+    @NotBlank
+    private String arrival;
+
+    @NotBlank
+    private String depAirport;
+
+    @NotBlank
+    private String arrAirport;
+
+    @NotBlank
+    private double price;
+
+    public Flight(@NotBlank flightId,  @NotBlank departure, @NotBlank String arrival, @NotBlank String depAirport, @NotBlank String arrAirport, @NotBlank double price) {
+        this.flightId = flightId;
+        this.departure = departure;
+        this.arrival = arrival;
+        this.depAirport = depAirport;
+        this.arrAirport = arrAirport;
+        this.price = price;
+    }
+
+    public void setFlightId(Long flightId) { this.flightId = flightId; }
+
+    public Long getFlightId() {return this.flightId; }
+
+    public void setDeparture(String departure) { this.departure = departure; }
+
+    public String getDeparture() { return this.departure; }
+
+    public void setArrival(String arrival) { this.arrival = arrival;}
+
+    public String getArrival() { return this.arrival; }
+
+    public void setDepAirport(String depAirport) { this.depAirport = depAirport; }
+
+    public String getDepAirport() { return this.depAirport;}
+
+    public void setArrAirport(String arrAirport) {this.arrAirport = arrAirport; }
+
+    public String getArrAirport() { return this.arrAirport; } 
+
+    public void setPrice(double price) { this.price = price; }
+
+    public double getPrice() { return this.price;}
+}
---
+++ src/main/resources/flight.sql
@@ -0,0 +1,8 @@
+CREATE TABLE flights (
+  flight_id INT AUTO_INCREMENT PRIMARY KEY,
+  departure DATETIME NOT NULL,
+  arrival DATETIME NOT NULL,
+  depAirport varchar(100) NOT NULL,
+  arrAirport varchar(100) NOT NULL,
+  price DOUBLE NOT NULL
+);
