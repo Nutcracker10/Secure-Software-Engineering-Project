9e2e7103eb0e70951bf43d1e140ff55c9a5faf57
Caoimhe Tiernan <caoimhe.tiernan.work@gmail.com>
2021-02-11 11:39:04 +0000

    updated reservation code. Added user object, added get for days and flight.

--- src/main/java/ie/ucd/dfh/model/Reservation.java
+++ src/main/java/ie/ucd/dfh/model/Reservation.java
@@ -4,22 +4,28 @@ package ie.ucd.dfh.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Date;
+import java.util.Calendar;
 
 @Entity
 public class Reservation {
     @Id
     @GeneratedValue
     private long reservationid;
-    private String nameOfReserver;
+    private User reserver;
     private String flightCode;
-    private Date dayOfFlight;
+    private Calendar dayOfFlight;
 
-    public Reservation(String nameOfReserver, String flightCode, Date dayOfFlight) {
-        this.nameOfReserver = nameOfReserver;
+    public Reservation(User reserver, String flightCode, Calendar dayOfFlight) {
+        this.reserver = reserver;
         this.flightCode = flightCode;
         this.dayOfFlight = dayOfFlight;
     }
 
+    public String getFlightCode(){
+        return flightCode;
+    }
 
+    public Calendar getDayOfFlight(){
+        return dayOfFlight;
+    }
 }
