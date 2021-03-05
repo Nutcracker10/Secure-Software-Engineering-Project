8c1081e5a392a1c0ed01bb9cabf26015bf17c5d9
James Kirwan <james.kirwan1@ucdconnect.ie>
2021-03-05 14:08:42 +0000

    added credit card details to book-flight method

--- src/main/java/ie/ucd/dfh/controller/ReservationController.java
+++ src/main/java/ie/ucd/dfh/controller/ReservationController.java
@@ -3,16 +3,15 @@ package ie.ucd.dfh.controller;
 import java.io.IOException;
 import java.util.Optional;
 
-import ie.ucd.dfh.model.Status;
+import ie.ucd.dfh.model.*;
+import ie.ucd.dfh.repository.CreditCardRepository;
+import ie.ucd.dfh.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import ie.ucd.dfh.UserSession;
-import ie.ucd.dfh.model.Flight;
-import ie.ucd.dfh.model.User;
-import ie.ucd.dfh.model.Reservation;
 import ie.ucd.dfh.repository.FlightRepository;
 import ie.ucd.dfh.repository.ReservationRepository;
 
@@ -25,15 +24,31 @@ public class ReservationController {
     private UserSession userSession;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CreditCardRepository creditCardRepository;
+
+    @Autowired
     private ReservationRepository reservationRepository;
 
     @Autowired
     private FlightRepository flightrepository;
 
     @PostMapping(value="/book-flight")
-    public void bookFlight(Long flightId, String firstName, String lastName, String homeAddress, String phonenumber, String email, HttpServletResponse response) throws IOException {
+    public void bookFlight(Long flightId, String firstName, String lastName, String homeAddress, String phonenumber, String email,
+                           String cardType, String cardNumber, String expiryMonth, String expiryYear, String securityCode, HttpServletResponse response) throws IOException {
 
         Optional<Flight> flight = flightrepository.findFlightById(flightId);
+
+        // if user is guest, create record of guest and payment
+        if (userSession.getUser() == null) {
+            User user = new User(firstName, lastName, homeAddress, phonenumber, email, null, "guest");
+            CreditCard creditCard = new CreditCard(cardType, cardNumber, expiryMonth, expiryYear, securityCode);
+            userRepository.save(user);
+            user.setCreditCard(creditCard);
+            creditCardRepository.save(creditCard);
+        }
         
         if (flight.isPresent()) {
             Reservation reservation = new Reservation(flight.get(), firstName, lastName, homeAddress, phonenumber, email);
