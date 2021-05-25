163ae1064865329764081c32f20db53d1131fa8e
Caoimhe Tiernan <caoimhe.tiernan.work@gmail.com>
2021-05-25 13:13:15 +0100

    admin can delete flights

--- src/main/java/ie/ucd/dfh/repository/ReservationRepository.java
+++ src/main/java/ie/ucd/dfh/repository/ReservationRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ie.ucd.dfh.model.Reservation;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Repository
+@Transactional
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {    
 
 }
