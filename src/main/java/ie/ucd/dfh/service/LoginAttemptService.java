d4b9e013e384d94212b09a310397febf55896d67
James Kirwan <james.kirwan1@ucdconnect.ie>
2021-05-25 17:57:41 +0100

    Classes finished, need to be implemeneted

--- src/main/java/ie/ucd/dfh/service/LoginAttemptService.java
+++ src/main/java/ie/ucd/dfh/service/LoginAttemptService.java
@@ -11,13 +11,13 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class LoginAttemptService {
 
-    private final int MAX_ATTEMPT = 10;
+    private final int MAX_ATTEMPT = 3;
     private LoadingCache<String, Integer> attemptsCache;
 
     public LoginAttemptService() {
         super();
         attemptsCache = CacheBuilder.newBuilder().
-                expireAfterWrite(1, TimeUnit.DAYS).build(new CacheLoader<String, Integer>() {
+                expireAfterWrite(20, TimeUnit.MINUTES).build(new CacheLoader<String, Integer>() {
             public Integer load(String key) {
                 return 0;
             }
